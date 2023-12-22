c245dc53e33b931e18fc21d587fe0904ca708fde
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2023-12-22 08:01:02 +0100

    Add custom filter

--- optuna/importance/filters/__init__.py
+++ optuna/importance/filters/__init__.py
@@ -1,7 +1,8 @@
 from optuna.importance.filters._base import BaseFilter
-from optuna.importance.filters._cufoff_filter import CutOffFilter
+from optuna.importance.filters._custom_filter import CustomFilter
+from optuna.importance.filters._cutoff_filter import CutOffFilter
 from optuna.importance.filters._quantile_filter import QuantileFilter
 from optuna.importance.filters._topk_filter import TopKFilter
 
 
-__all__ = ["BaseFilter", "CutOffFilter", "QuantileFilter", "TopKFilter"]
+__all__ = ["BaseFilter", "CustomFilter", "CutOffFilter", "QuantileFilter", "TopKFilter"]
