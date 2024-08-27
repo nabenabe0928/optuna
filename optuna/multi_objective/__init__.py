ca456ea1a0a448102a393b64e46dd089af049eac
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-08-27 07:45:15 +0200

    Remove the multi_objective directory to avoid confusion

--- optuna/multi_objective/__init__.py
+++
@@ -1,6 +0,0 @@
-# TODO(nabenabe0928): Come up with any ways to remove this file.
-# NOTE(nabenabe0928): Discuss when to remove this class.
-raise ModuleNotFoundError(
-    "`optuna.multi_objective` was removed at v4.0.0. "
-    "Please update your code or downgrade your Optuna version."
-)
