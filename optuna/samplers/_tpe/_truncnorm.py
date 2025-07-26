5e371bcd792af04fe1d5c4cb8cdb11bcd640b8c3
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2025-07-26 10:00:47 +0200

    Merge master [2/n]

--- optuna/samplers/_tpe/_truncnorm.py
+++ optuna/samplers/_tpe/_truncnorm.py
@@ -121,7 +121,7 @@ def _log_gauss_mass(a: np.ndarray, b: np.ndarray) -> np.ndarray:
     # Calculations in right tail are inaccurate, so we'll exploit the
     # symmetry and work only in the left tail
     right_inds = np.nonzero(a > 0)
-    a[*right_inds], b[*right_inds] = -b[*right_inds], -a[*right_inds]
+    a[right_inds], b[right_inds] = -b[right_inds], -a[right_inds]
     left_inds = np.nonzero(b <= 0)
     central_inds = np.nonzero(b > 0)
 
@@ -144,9 +144,9 @@ def _log_gauss_mass(a: np.ndarray, b: np.ndarray) -> np.ndarray:
     # _lazyselect not working; don't care to debug it
     out = np.full_like(a, fill_value=np.nan, dtype=np.complex128)
     if left_inds[0].size:
-        out[*left_inds] = mass_case_left(a[*left_inds], b[*left_inds])
+        out[left_inds] = mass_case_left(a[left_inds], b[left_inds])
     if central_inds[0].size:
-        out[*central_inds] = mass_case_central(a[*central_inds], b[*central_inds])
+        out[central_inds] = mass_case_central(a[central_inds], b[central_inds])
     return np.real(out)  # discard ~0j
 
 
@@ -262,22 +262,22 @@ def ppf(q: np.ndarray, a: np.ndarray | float, b: np.ndarray | float) -> np.ndarr
 
     case_left = a < 0
     case_right = ~case_left
-
-    def ppf_left(q: np.ndarray, a: np.ndarray, b: np.ndarray) -> np.ndarray:
-        log_Phi_x = _log_sum(_log_ndtr_negative(a), np.log(q) + _log_gauss_mass(a, b))
+    log_mass = _log_gauss_mass(a, b)
+    def ppf_left(q: np.ndarray, a: np.ndarray, b: np.ndarray, log_mass: np.ndarray) -> np.ndarray:
+        log_Phi_x = _log_sum(_log_ndtr_negative(a), np.log(q) + log_mass)
         return _ndtri_exp(log_Phi_x)
 
-    def ppf_right(q: np.ndarray, a: np.ndarray, b: np.ndarray) -> np.ndarray:
+    def ppf_right(q: np.ndarray, a: np.ndarray, b: np.ndarray, log_mass: np.ndarray) -> np.ndarray:
         # NOTE(nabenabe): Since the numerical stability of log_ndtr is better in the left tail, we
         # flip the side for a >= 0.
-        log_Phi_x = _log_sum(_log_ndtr_negative(-b), np.log1p(-q) + _log_gauss_mass(a, b))
+        log_Phi_x = _log_sum(_log_ndtr_negative(-b), np.log1p(-q) + log_mass)
         return -_ndtri_exp(log_Phi_x)
 
     out = np.empty_like(q)
     if (q_left := q[case_left]).size:
-        out[case_left] = ppf_left(q_left, a[case_left], b[case_left])
+        out[case_left] = ppf_left(q_left, a[case_left], b[case_left], log_mass[case_left])
     if (q_right := q[case_right]).size:
-        out[case_right] = ppf_right(q_right, a[case_right], b[case_right])
+        out[case_right] = ppf_right(q_right, a[case_right], b[case_right], log_mass[case_right])
 
     out[q == 0] = a[q == 0]
     out[q == 1] = b[q == 1]
