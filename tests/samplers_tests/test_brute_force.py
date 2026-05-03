8a9db30e9fb42add910a60fc67546b64bbd00de6
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2026-05-03 21:05:22 +0900

    mypy

--- tests/samplers_tests/test_brute_force.py
+++ tests/samplers_tests/test_brute_force.py
@@ -23,15 +23,15 @@ def test_tree_node_add_paths() -> None:
         if leaf.children is None:
             leaf.set_leaf()
 
-    set_leaf_kwargs = dict(param_name=None, children={}, choices_fingerprint=(0, 0, 0))
+    _zs = (0, 0, 0)
     assert tree == _TreeNode(
         param_name="a",
         children={
             0: _TreeNode(
                 param_name="b",
                 children={
-                    0.0: _TreeNode(**set_leaf_kwargs),
-                    1.0: _TreeNode(**set_leaf_kwargs),
+                    0.0: _TreeNode(param_name=None, children={}, choices_fingerprint=_zs),
+                    1.0: _TreeNode(param_name=None, children={}, choices_fingerprint=_zs),
                 },
                 choices_fingerprint=(2, 0.0, 1.0),
             ),
@@ -41,7 +41,7 @@ def test_tree_node_add_paths() -> None:
                     0.0: _TreeNode(
                         param_name="c",
                         children={
-                            0: _TreeNode(**set_leaf_kwargs),
+                            0: _TreeNode(param_name=None, children={}, choices_fingerprint=_zs),
                             1: _TreeNode(),
                         },
                         choices_fingerprint=(2, 0, 1),
