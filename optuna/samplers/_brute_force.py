2d306046ff3ad79ebeecb022b4b4e573a499dbca
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2026-05-04 15:38:39 +0900

    Update brute force

--- optuna/samplers/_brute_force.py
+++ optuna/samplers/_brute_force.py
@@ -110,7 +110,7 @@ class _TreeNode:
 
 @experimental_class("3.1.0")
 class BruteForceSampler(BaseSampler):
-    """Sampler using brute force.
+    """Sampler that performs exhaustive search over the define-by-run search space.
 
     This sampler performs exhaustive search on the defined search space.
 
